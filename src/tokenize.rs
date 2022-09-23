3b6c5730cd4c276b2c6d9747f6493f20798fbc2b
koyashiro <develop@koyashi.ro>
2022-09-23 23:39:36 +0900

    feat: support for negative numbers, decimal points and floating point expressions

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -16,74 +16,103 @@ pub enum Token {
 }
 
 pub fn tokenize(input: &str) -> Result<Vec<Token>, Error> {
+    let mut tokens = Vec::new();
+
     let chars = input.chars().collect::<Vec<char>>();
     let mut p = chars.as_slice();
 
-    let mut tokens = Vec::new();
-
-    loop {
-        match p.first() {
-            Some(' ' | '\t' | '\n' | '\r') => p = &p[1..],
-            Some('[') => {
+    while let Some(c) = p.first() {
+        match c {
+            ' ' | '\t' | '\n' | '\r' => {
+                p = &p[1..];
+            }
+            '[' => {
                 tokens.push(Token::BeginArray);
                 p = &p[1..];
             }
-            Some('{') => {
+            '{' => {
                 tokens.push(Token::BeginObject);
                 p = &p[1..];
             }
-            Some(']') => {
+            ']' => {
                 tokens.push(Token::EndArray);
                 p = &p[1..];
             }
-            Some('}') => {
+            '}' => {
                 tokens.push(Token::EndObject);
                 p = &p[1..];
             }
-            Some(':') => {
+            ':' => {
                 tokens.push(Token::NameSeparator);
                 p = &p[1..];
             }
-            Some(',') => {
+            ',' => {
                 tokens.push(Token::ValueSeparator);
                 p = &p[1..];
             }
-            Some('"') => {
+            '"' => {
                 p = &p[1..];
 
                 let mut s = String::new();
                 loop {
                     match p.first() {
                         Some('"') => {
-                            p = &p[1..];
                             tokens.push(Token::String(s));
+                            p = &p[1..];
                             break;
                         }
                         Some(c) => {
                             s.push(*c);
                             p = &p[1..];
                         }
-                        None => return Err(Error),
+                        None => {
+                            return Err(Error);
+                        }
                     }
                 }
             }
-            Some('0'..='9') => {
+            '-' | '0'..='9' => {
                 let mut s = String::new();
-                loop {
-                    match p.first() {
-                        Some(c) if c.is_digit(10) => {
-                            s.push(*c);
-                            p = &p[1..];
-                        }
-                        _ => {
-                            let n = s.parse().unwrap();
-                            tokens.push(Token::Number(n));
-                            break;
-                        }
+
+                if let Some(c @ '-') = p.first() {
+                    s.push(*c);
+                    p = &p[1..];
+                }
+
+                while let Some(c @ '0'..='9') = p.first() {
+                    s.push(*c);
+                    p = &p[1..];
+                }
+
+                if let Some(c @ '.') = p.first() {
+                    s.push(*c);
+                    p = &p[1..];
+
+                    while let Some(c @ '0'..='9') = p.first() {
+                        s.push(*c);
+                        p = &p[1..];
                     }
                 }
+
+                if let Some(c @ 'e') = p.first() {
+                    s.push(*c);
+                    p = &p[1..];
+
+                    if let Some(c @ '-') = p.first() {
+                        s.push(*c);
+                        p = &p[1..];
+                    }
+
+                    while let Some(c @ '0'..='9') = p.first() {
+                        s.push(*c);
+                        p = &p[1..];
+                    }
+                }
+
+                let n = s.parse().unwrap();
+                tokens.push(Token::Number(n));
             }
-            Some('f') => {
+            'f' => {
                 if let Some(['f', 'a', 'l', 's', 'e']) = &p.get(0..5) {
                     tokens.push(Token::False);
                     p = &p[5..];
@@ -91,7 +120,7 @@ pub fn tokenize(input: &str) -> Result<Vec<Token>, Error> {
                     return Err(Error);
                 }
             }
-            Some('n') => {
+            'n' => {
                 if let Some(['n', 'u', 'l', 'l']) = &p.get(0..4) {
                     tokens.push(Token::Null);
                     p = &p[4..];
@@ -99,7 +128,7 @@ pub fn tokenize(input: &str) -> Result<Vec<Token>, Error> {
                     return Err(Error);
                 }
             }
-            Some('t') => {
+            't' => {
                 if let Some(['t', 'r', 'u', 'e']) = &p.get(0..4) {
                     tokens.push(Token::True);
                     p = &p[4..];
@@ -107,8 +136,9 @@ pub fn tokenize(input: &str) -> Result<Vec<Token>, Error> {
                     return Err(Error);
                 }
             }
-            None => break,
-            _ => return Err(Error),
+            _ => {
+                return Err(Error);
+            }
         }
     }
 
@@ -132,6 +162,38 @@ mod tests {
         assert_eq!(tokenize("null"), Ok(vec![Token::Null]));
         assert_eq!(tokenize("true"), Ok(vec![Token::True]));
         assert_eq!(tokenize("12345"), Ok(vec![Token::Number(12345f64)]));
+        assert_eq!(tokenize("12345e123"), Ok(vec![Token::Number(12345e123f64)]));
+        assert_eq!(
+            tokenize("12345e-123"),
+            Ok(vec![Token::Number(12345e-123f64)])
+        );
+        assert_eq!(tokenize("123.45"), Ok(vec![Token::Number(123.45f64)]));
+        assert_eq!(
+            tokenize("123.45e123"),
+            Ok(vec![Token::Number(123.45e123f64)])
+        );
+        assert_eq!(
+            tokenize("123.45e-123"),
+            Ok(vec![Token::Number(123.45e-123f64)])
+        );
+        assert_eq!(tokenize("-12345"), Ok(vec![Token::Number(-12345f64)]));
+        assert_eq!(
+            tokenize("-12345e123"),
+            Ok(vec![Token::Number(-12345e123f64)])
+        );
+        assert_eq!(
+            tokenize("-12345e-123"),
+            Ok(vec![Token::Number(-12345e-123f64)])
+        );
+        assert_eq!(tokenize("-123.45"), Ok(vec![Token::Number(-123.45f64)]));
+        assert_eq!(
+            tokenize("-123.45e123"),
+            Ok(vec![Token::Number(-123.45e123f64)])
+        );
+        assert_eq!(
+            tokenize("-123.45e-123"),
+            Ok(vec![Token::Number(-123.45e-123f64)])
+        );
         assert_eq!(
             tokenize("\"string\""),
             Ok(vec![Token::String("string".to_string())])
